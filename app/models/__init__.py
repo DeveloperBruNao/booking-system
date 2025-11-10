c698ebd9106ca05cfe4eaf4040b81ed5dcf9b8e5
Bruno Damasceno DevRPA <eubruno.rpa@gmail.com>
2025-11-10 16:29:07 -0300

    feat: adiciona model de reservas com status

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.space import Space
+from app.models.booking import Booking, BookingStatus
+
+__all__ = ["User", "Space", "Booking", "BookingStatus"]
